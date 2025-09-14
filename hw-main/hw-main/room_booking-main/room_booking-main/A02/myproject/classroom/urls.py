6024aa6f739e25be20ee63573e7201741d763e34
Lalita Thatsananunchai <6710615243@student.tu.ac.th>
2025-09-15 04:46:56 +0700

    อันล่าสุด

--- hw-main/hw-main/room_booking-main/room_booking-main/A02/myproject/classroom/urls.py
+++ hw-main/hw-main/room_booking-main/room_booking-main/A02/myproject/classroom/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('room/', views.room, name='room'),
     path('book/<int:id>/', views.room, name='book_room'),
-    path('my_bookings/', views.my_bookings, name='my_bookings'),
+    path('my_booking/', views.my_booking, name='my_booking'),
     path('cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
 
     path('login/', views.login_user, name='login_user'),
--- hw-main/hw-main/room_booking-main/room_booking-main/A02/myproject/classroom/views.py
+++ hw-main/hw-main/room_booking-main/room_booking-main/A02/myproject/classroom/views.py
@@ -14,17 +14,17 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            messages.success(request, "เข้าสู่ระบบสำเร็จ")
+            messages.success(request, "เข้าสู่ระบบสำเร็จ", extra_tags="login")
             return redirect('index')  # หรือหน้า dashboard ของคุณ
         else:
-            messages.error(request, "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง")
+            messages.error(request, "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง",  extra_tags="login")
     
     return render(request, 'rooms/login.html')
 
 def logout_user(request):
     logout(request)
     messages.success(request, "ออกจากระบบเรียบร้อย")
-    return redirect('login_user')
+    return redirect('index')
 
 
 def index(request):
@@ -34,18 +34,25 @@ def index(request):
 
 def room(request,id):
     room = RoomData.objects.get(id=id)
+    
+    existing_booking = Booking.objects.filter(user=request.user).first()
+    if existing_booking:
+        messages.error(request, "คุณมีการจองห้องอยู่แล้ว กรุณายกเลิกก่อนจองใหม่")
+        return redirect('my_booking')
+    
     if room.available_hours > 0:
         Booking.objects.create(user=request.user, room_user=room, hours=1)
         room.available_hours -= 1
         room.save()
-        messages.success(request, f'จองห้อง {room.room_name} สำเร็จ!')
+        messages.success(request, f'จองห้อง {room.room_name} สำเร็จ!', extra_tags="booking")
     else:
-        messages.error(request, f'ห้อง {room.room_name} ไม่สามารถจองได้ เนื่องจากไม่มีชั่วโมงว่างแล้ว!')
+        messages.error(request, f'ห้อง {room.room_name} ไม่สามารถจองได้ เนื่องจากไม่มีชั่วโมงว่างแล้ว!', extra_tags="booking")
+
     return redirect('index')
 
-def my_bookings(request):
+def my_booking(request):
     bookings = Booking.objects.filter(user=request.user)
-    return render(request, 'classroom/my_bookings.html', {'bookings': bookings})
+    return render(request, 'rooms/my_booking.html', {'bookings': bookings})
 
 def cancel_booking(request, booking_id):
     booking = Booking.objects.get(id=booking_id, user=request.user)
@@ -54,7 +61,8 @@ def cancel_booking(request, booking_id):
     room.save()
     booking.delete()
     messages.success(request, f'ยกเลิกการจอง {room.room_name} เรียบร้อยแล้ว')
-    return redirect('my_bookings')
+    return redirect('my_booking')
+
 
 def login_admin(request):
     return redirect('admin')
