a553619fb48010d76a42ee8a7b72425447d4a2ff
lalita <6710615243@gmail.com>
2025-11-08 00:07:01 +0700

    test

--- room_booking-main/room_booking-main/A02/myproject/myproject/urls.py
+++ room_booking-main/room_booking-main/A02/myproject/myproject/urls.py
@@ -21,7 +21,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('classroom.urls'))
-    # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    # path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    # path('', include('classroom.urls')),
 ]
